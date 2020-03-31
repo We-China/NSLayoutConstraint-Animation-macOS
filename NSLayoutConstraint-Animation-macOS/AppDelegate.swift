ca45c921fb4596366875e1c3d76f7cb3a88cd84b
aeing <12443771@qq.com>
2020-04-01 00:18:19 +0800

    Project init

---
+++ NSLayoutConstraint-Animation-macOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NSLayoutConstraint-Animation-macOS
+//
+//  Created by zuolin on 2020/4/1.
+//  Copyright © 2020 zuolin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ NSLayoutConstraint-Animation-macOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NSLayoutConstraint-Animation-macOS
+//
+//  Created by zuolin on 2020/4/1.
+//  Copyright © 2020 zuolin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
