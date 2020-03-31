5cd332a76050aa4e3e976a895c99b8e354555726
aeing <12443771@qq.com>
2020-04-01 00:34:51 +0800

    Update Layout Constraint animation

--- NSLayoutConstraint-Animation-macOS/ViewController.swift
+++ NSLayoutConstraint-Animation-macOS/ViewController.swift
@@ -10,10 +10,22 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet private weak var contentView: NSView!
+    
+    @IBOutlet private weak var contentViewTopSpaceLayoutConstraint: NSLayoutConstraint!
+    
+    private var contentViewTrackingArea: NSTrackingArea?
+    
+    private let minTopConstant: CGFloat = -48.0
+    private let maxTopConstant: CGFloat = .zero
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        updateContentView()
+        contentViewAddTrackingArea()
     }
 
     override var representedObject: Any? {
@@ -21,7 +33,65 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
+    
+    override func mouseEntered(with event: NSEvent) {
+        super.mouseEntered(with: event)
+        
+        updateLayourConstraintWhenMouseEntered()
+    }
+    
+    override func mouseExited(with event: NSEvent) {
+        super.mouseExited(with: event)
+     
+        updateLayourConstraintWhenMouseExited()
+    }
+    
+    deinit {
+        contentViewRemoveTrackingArea()
+    }
+}
 
-
+extension ViewController {
+    private func updateContentView() {
+        contentView.wantsLayer = true
+        
+        if let layer = contentView.layer {
+            layer.backgroundColor = NSColor.controlAccentColor.cgColor
+        }
+    }
+    
+    private func contentViewAddTrackingArea() {
+        contentViewTrackingArea = NSTrackingArea(rect: contentView.bounds, options: [.activeAlways, .inVisibleRect, .mouseEnteredAndExited], owner: self, userInfo: nil)
+        
+        guard let trackingArea = contentViewTrackingArea else {
+            return
+        }
+        
+        contentView.addTrackingArea(trackingArea)
+    }
+    
+    private func contentViewRemoveTrackingArea() {
+        guard let trackingArea = contentViewTrackingArea else {
+            return
+        }
+        
+        contentView.removeTrackingArea(trackingArea)
+    }
+    
+    private func updateLayourConstraintWhenMouseEntered() {
+        if contentViewTopSpaceLayoutConstraint.priority != .defaultHigh {
+            contentViewTopSpaceLayoutConstraint.animator().constant = maxTopConstant
+        }
+        
+        view.layoutSubtreeIfNeeded()
+    }
+        
+    private func updateLayourConstraintWhenMouseExited() {
+        if contentViewTopSpaceLayoutConstraint.priority != .defaultHigh {
+            contentViewTopSpaceLayoutConstraint.animator().constant = minTopConstant
+        }
+        
+        view.layoutSubtreeIfNeeded()
+    }
 }
 
